0296a067c765d5756c52fbd64498ead017c3a6ec
Tamás Tóth <tothtamas28@users.noreply.github.com>
2026-05-15 22:03:30 +0200

    Run the `LibAFL`-based fuzzer (#12)
    * Update `skribe` to version `0.1.40`

--- test.sh
+++ test.sh
@@ -15,9 +15,12 @@ cd -
 
 cd skribe/skribe-fuzz-rs
 cargo test
+cargo build --release
+cp target/release/skribe-fuzz .
+time ./skribe-fuzz --iterations 100 --fuzz-spec fuzz-spec.json --contract-name TestSkribeEndToEnd --function-name test_end_to_end_intense
 
 cd fuzz
 cargo +nightly fuzz build
 cd ..
-cp target/x86_64-unknown-linux-gnu/release/fuzz_target_1 skribe-fuzz
-time ./skribe-fuzz -runs=100 --fuzz-spec=fuzz-spec.json --contract-name=TestSkribeEndToEnd --function-name=test_end_to_end_intense
+cp target/x86_64-unknown-linux-gnu/release/fuzz_target_1 skribe-fuzz-libfuzzer
+time ./skribe-fuzz-libfuzzer -runs=100 --fuzz-spec=fuzz-spec.json --contract-name=TestSkribeEndToEnd --function-name=test_end_to_end_intense
